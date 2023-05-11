c4c37c350eb7a658d4a095139e8366c7f8967f68
jcross <jcrossover1208@gmail.com>
2023-05-11 16:41:19 +0800

    twelveth commit: adding js to local mysql.

--- websiteJS.js
+++ websiteJS.js
@@ -2,3 +2,18 @@ function alertCall(){
     alert("Bazinga! nigga...");
     location.href="secondPage.html"
 }
+function connect(){
+    var mysql = require('mysql');
+
+    var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "johndada1208S"
+    });
+
+    con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    alert("connected!")
+    });
+}
