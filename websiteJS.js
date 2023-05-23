99c640db566d3b1328eb8fa5bd6c62492ae4d1e3
jcross <jcrossover1208@gmail.com>
2023-05-24 03:10:54 +0800

    added headers

--- websiteJS.js
+++ websiteJS.js
@@ -2,29 +2,16 @@ function alertCall(){
     alert("Bazinga! nigga...");
     location.href="secondPage.html"
 }
-function connect(){
-    var mysql = require('mysql');
-
-    var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "johndada1208S"
-    });
-
-    con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    alert("connected!")
-    });
-}
 function openMyselfTab(evt, stages) {
     // Declare all variables
     var i, tabcontent, tablinks;
   
-    // Get all elements with class="tabcontent" and hide them
+    // Get all elements with class="tabcontent"+"Tabs_HeaderContent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
+    Tabs_HeaderContent= document.getElementsByClassName("Tabs_HeaderContent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
+      Tabs_HeaderContent[i].style.display = "none"
     }
   
     // Get all elements with class="tablinks" and remove the class "active"
@@ -35,5 +22,6 @@ function openMyselfTab(evt, stages) {
   
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(stages).style.display = "block";
+    document.getElementById("H_"+stages).style.display = "block";
     evt.currentTarget.className += " active";
   }
